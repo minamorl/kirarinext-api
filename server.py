9e5a5afd8da46ee3210a6df3134fe3c7aa88b8da
minamorl <minamorl@users.noreply.github.com>
2016-05-22 01:42:42 +0900

    only return latest 50 comments

--- server.py
+++ server.py
@@ -20,16 +20,17 @@ app.permanent_session_lifetime = datetime.timedelta(days=7)
 p = Persistent("kirarinext")
 
 
-def find_comments(thread, fetch_from=None):
+def find_comments(thread, fetch_from=None, page=None):
+    COMMENT_PER_PAGE = 50
+    max_id = p.get_max_id(Comment)
+    if max_id is None:
+        return []
     if fetch_from is not None:
-        max_id = p.get_max_id(Comment)
-        if max_id is None:
-            return []
         ids = range(int(fetch_from) + 1, max_id + 1)
         return [p.load(Comment, _id) for _id in ids]
-
-    return [comment for comment in p.load_all(Comment)
-            if comment.thread == thread.name]
+    # if both page and fetch_from are None, then return latest 50 comments.
+    ids = range(max_id + 1 - 50, max_id + 1)
+    return [p.load(Comment, _id) for _id in ids]
 
 
 def find_thread(thread_name, ensure_exists=True):
@@ -64,6 +65,7 @@ def comment():
 
 @app.route("/api/comments", methods=["GET"])
 def thread():
+    """returns latest 50 comments"""
     thread_name = "$DEFAULT"
     thread = find_thread(thread_name)
     comments = find_comments(thread, request.args.get("from"))
